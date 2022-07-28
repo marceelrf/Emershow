3a22d79e98556b3b2ae36b5cfe415cc4caa9507a
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2022-07-28 17:19:48 -0400

    commit emershow & mengo

--- scripts/modelo.R
+++ scripts/modelo.R
@@ -4,3 +4,17 @@
 # Rotulo de seção ---------------------------------------------------------
 
 1+1
+
+# Utilidades --------------------------------------------------------------
+
+# ctrl+shift+c
+# install.packages("devtools")
+# install.packages("usethis")
+# install.packages("tidyverse")
+
+# iniciando o git ---------------------------------------------------------
+
+library("usethis")
+
+usethis::use_git()
+usethis::use_github()
